df121e76a4a47224a0b186dca00bd2e0e0ac8dfc
ChrisFennek <christian.gebhardt@online.de>
2020-12-11 18:23:01 +0100

    toggleIcon

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 const toggleSwitch = document.querySelector("input[type=checkbox]");
+const nav = document.querySelector("#nav");
+const toggleIcon = document.querySelector("#toggle-icon");
+const image1 = document.querySelector("#image1");
+const image2 = document.querySelector("#image2");
+const image3 = document.querySelector("#image3");
+const textBox = document.querySelector("#text-box");
+
+function darkMode() {
+  nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
+  textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
+  toggleIcon.children[0].textContent = "Dark Mode";
+  toggleIcon.children[1].classList.remove("fa-sun");
+  toggleIcon.children[1].classList.add("fa-moon");
+}
 
 function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
+    darkMode();
   } else {
     document.documentElement.setAttribute("data-theme", "light");
+    lightMode();
   }
 }
 
